ed7d40fe1f9df1a47b746e66d993f34ca3aec695
Michael Risher <rishermichael@gmail.com>
2023-06-09 17:06:34 -0700

    added a most booked from country map reduce too

--- mapreduce/mapreduce/src/main/java/com/mapreduce/App.java
+++ mapreduce/mapreduce/src/main/java/com/mapreduce/App.java
@@ -21,21 +21,42 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class App 
 {
     public static void main( String[] args ) throws Exception {
+        Path inPath = new Path( args[ 0 ] );
+        Path profitPath = new Path( args[1] + "Price" );
+        Path countriesPath = new Path( args[1] + "Country" );
+
+        System.out.println( "starting job1 with input: " + inPath + " and output as: " + profitPath );
+
 		Configuration conf = new Configuration();
-		Job job = Job.getInstance( conf, "word count" );
+		Job job = Job.getInstance( conf, "Month Profit" );
 		job.setJarByClass( App.class );
-		job.setMapperClass( TokenizerMapper.class );
+		job.setMapperClass( MonthProfitMapper.class );
 		job.setCombinerClass( IntSumReducer.class );
 		job.setReducerClass( IntSumReducer.class );
 		job.setOutputKeyClass( Text.class );
 		job.setOutputValueClass( FloatWritable.class );
-        System.out.println( args[0] );
-		FileInputFormat.addInputPath( job, new Path( args[ 0 ] ) );
-		FileOutputFormat.setOutputPath( job, new Path( args[ 1 ] ) );
-		System.exit( job.waitForCompletion( true ) ? 0 : 1 );
+		FileInputFormat.addInputPath( job, inPath );
+		FileOutputFormat.setOutputPath( job, profitPath );
+		// System.exit( job.waitForCompletion( true ) ? 0 : 1 );
+        job.waitForCompletion( true );
+
+
+        System.out.println( "starting job2 with input: " + inPath + " and output as: " + countriesPath );
+
+		Job job2 = Job.getInstance( conf, "Month Profit" );
+		job2.setJarByClass( App.class );
+		job2.setMapperClass( CountryProfitMapper.class );
+		job2.setCombinerClass( IntSumReducer.class );
+		job2.setReducerClass( IntSumReducer.class );
+		job2.setOutputKeyClass( Text.class );
+		job2.setOutputValueClass( FloatWritable.class );
+        
+		FileInputFormat.addInputPath( job2, inPath );
+		FileOutputFormat.setOutputPath( job2, countriesPath );
+		System.exit( job2.waitForCompletion( true ) ? 0 : 1 );
 	}
 
-    public static class TokenizerMapper extends Mapper<Object, Text, Text, FloatWritable> {
+    public static class MonthProfitMapper extends Mapper<Object, Text, Text, FloatWritable> {
 		private Text month = new Text();
         private FloatWritable profit = new FloatWritable();
 
@@ -62,6 +83,34 @@ public class App
 		}
 	}
 
+    public static class CountryProfitMapper extends Mapper<Object, Text, Text, FloatWritable> {
+        private Text countryText  = new Text(); 
+        private FloatWritable profit = new FloatWritable();
+
+        public void map( Object key, Text value, Context context ) throws IOException, InterruptedException {
+            // Split the CSV line into columns
+            String[] columns = value.toString().split(",");
+            // Extract relevant columns
+            String bookingStatus = columns[0];
+            try{
+                String country = columns[6];
+                float price = Float.parseFloat(columns[7]);
+                // filter out record with no country
+                if( !country.isBlank() && !country.isEmpty() ){
+                    // Filter out non-booked entries
+                    if ( bookingStatus.equals( "0" ) ) {
+                        // Emit the month and profit as key-value pairs
+                        countryText.set( country );
+                        profit.set( price );
+                        context.write( countryText, profit );
+                    }
+                }
+            } catch( NumberFormatException e ){
+                // ignore
+                // Need this because the csv has a header line which it attempts to read so when converting to a float it fails to here
+            }
+        }
+    }
     public static class IntSumReducer extends Reducer<Text, FloatWritable, Text, FloatWritable> {
 		private FloatWritable totalProfit = new FloatWritable();
 
